474fe8671c81fe42269fb8e56ebfa7361957b005
chax235 <32308811+chax235@users.noreply.github.com>
2017-11-26 00:47:14 -0500

    Update Server.java

--- src/part2/Server.java
+++ src/part2/Server.java
@@ -6,6 +6,12 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+import java.net.ServerSocket;
+import java.io.File;
+import javax.imageio.ImageIO;
+import  java.awt.image.BufferedImage;
+
+
 public class Server implements Runnable {
 
     private String name;
@@ -13,6 +19,8 @@ public class Server implements Runnable {
 
     private String routerIP;
     private String routerPort;
+
+    private String imagePath; //path to jpg image file to send
 
     @Override
     public void run() {
@@ -74,6 +82,38 @@ public class Server implements Runnable {
 
     private void acceptClientCommunication() {
         //TODO Logic for the server actually accepting client connections and doing stuff goes here
+
+        switch(getName()){
+            case "imageServer"://send image
+            {
+
+                    //code not working
+               /* BufferedImage image = ImageIO.read(new File("test.jpg"));
+                try (ServerSocket serv = new ServerSocket(getPort())) {
+                    System.out.println("waiting...");
+                    try (Socket socket = serv.accept()) {
+                        System.out.println("client connected");
+                        ImageIO.write(image, "jpg", socket.getOutputStream());
+                        System.out.println("sent");
+
+                    }
+                }*/
+            }
+                break;
+            case "textServer"://send text
+            {
+
+            }
+                break;
+            case "audioServer"://send audio
+            {
+
+            }
+                break;
+
+        }
+
+               // socket.close;
     }
 
     public static void main(String[] args) throws IOException {
@@ -82,6 +122,28 @@ public class Server implements Runnable {
         server.setPort("5655");
         server.setRouterIP("127.0.0.1");
         server.setRouterPort("5555");
+
+        Server serverImage = new Server();
+        server.setName("imageServer");
+        server.setPort("5656");
+        server.setRouterIP("127.0.0.1");
+        server.setRouterPort("5555");
+
+        Server serverText = new Server();
+        server.setName("textServer");
+        server.setPort("5657");
+        server.setRouterIP("127.0.0.1");
+        server.setRouterPort("5555");
+
+        Server serverAudio = new Server();
+        server.setName("audioServer");
+        server.setPort("5658");
+        server.setRouterIP("127.0.0.1");
+        server.setRouterPort("5555");
+
         new Thread(server).start();
+        new Thread(serverImage).start();
+        new Thread(serverText).start();
+        new Thread(serverAudio).start();
     }
 }
